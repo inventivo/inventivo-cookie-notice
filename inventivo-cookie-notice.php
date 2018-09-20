62c95ac227b1f27c0918349df61060414090e229
inventivode <inventivode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-09-20 04:35:14 +0000

    moved to github
    git-svn-id: https://plugins.svn.wordpress.org/inventivo-cookie-notice/trunk@1944022 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inventivo-cookie-notice.php
+++ inventivo-cookie-notice.php
@@ -3,13 +3,13 @@ Contributors: inventivogermany
 Plugin Name:  Cookie Notice GDPR | inventivo
 Plugin URI:   https://www.inventivo.de/wordpress-agentur/wordpress-plugins
 Description:  Display the EU Cookie Notice in a popup (EU Cookie Guideline)
-Version:      0.2.6
+Version:      0.2.7
 Author:       Nils Harder
 Author URI:   https://www.inventivo.de
 Tags: cookie notice, cookie hinweis, eu cookie richtlinie, cookie popup, inventivo, gdpr, dsgvo
 Requires at least: 3.0
 Tested up to: 4.9.7
-Stable tag: 0.2.6
+Stable tag: 0.2.7
 Text Domain: inventivo-cookie-notice
 Domain Path: /languages
 License:      GPL2
